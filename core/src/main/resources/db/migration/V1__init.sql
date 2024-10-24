ede36868ae99dfe762e311105330f56964dc705f
ogbozoyan <ogbozoyan@mail.ru>
2024-10-24 14:57:54 +0300

    init

---
+++ core/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,16 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlResolveForFile
+
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS vector_store
+(
+    id        uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    content   text,
+    metadata  json,
+    embedding vector(1024) -- for nomic-embed-text size equals 768, it may violate of Embeding model
+);
+
+CREATE INDEX ON vector_store USING HNSW (embedding vector_cosine_ops);
