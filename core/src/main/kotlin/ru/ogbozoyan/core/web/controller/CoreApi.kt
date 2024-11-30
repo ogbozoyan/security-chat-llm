5ac933bd60b22df1cc036f8d55ec304f7e86ebf5
ogbozoyan <ogbozoyan@mail.ru>
2024-11-30 15:45:30 +0300

    add: streaming response; jpa buddy removed: websocket todo: frontend

--- core/src/main/kotlin/ru/ogbozoyan/core/web/controller/CoreApi.kt
+++ core/src/main/kotlin/ru/ogbozoyan/core/web/controller/CoreApi.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -32,9 +33,10 @@ interface CoreApi {
     @Operation(summary = "Ask question to llm and save chat", description = "Returns the result")
     fun embedFile(file: MultipartFile, type: FileTypeEnum)
 
-    @PostMapping(
+    @GetMapping(
         "/api/v1/chat",
-        consumes = [MediaType.APPLICATION_JSON_VALUE]
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.TEXT_EVENT_STREAM_VALUE]
     )
     @Operation(summary = "Ask question to llm and save chat (STREAM)", description = "Returns future of parts")
     @ResponseStatus(HttpStatus.OK)
